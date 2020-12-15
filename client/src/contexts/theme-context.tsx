3f3f77da33339bc1e631836751748de87bb0e13a
alex-greff <alex.j.greff@gmail.com>
2020-12-14 22:48:28 -0500

    Made theme changes look smoother

--- client/src/contexts/theme-context.tsx
+++ client/src/contexts/theme-context.tsx
@@ -3,6 +3,8 @@ import useLocalStorage from "@/hooks/useLocalStorage";
 import { ThemeID } from "@/types";
 import { defaultTheme } from "@/constants/themes";
 
+const THEME_CHANGE_DURATION = 200;
+
 interface ThemeContextValue {
   theme: ThemeID | null;
   switchTheme: (newTheme: string) => void;
@@ -16,10 +18,20 @@ const ThemeContext = React.createContext<ThemeContextValue>({
 export const ThemeProvider: FunctionComponent = ({ children }) => {
   const [theme, setTheme] = useLocalStorage("theme", null);
   const switchTheme = (newTheme: string) => {
+    // Add the theme change class to make the theme change to smoothly
+    const gatsbyRoot = document.querySelector("#___gatsby");
+    gatsbyRoot?.classList.add("theme-change");
+
+    // Change the theme
     const root = document.body;
     root.classList.remove(theme);
     root.classList.add(newTheme);
     setTheme(newTheme);
+
+    setTimeout(() => {
+      // Theme change animation is done, remove the class
+      gatsbyRoot?.classList.remove("theme-change");
+    }, THEME_CHANGE_DURATION);
   };
 
   // Set the current theme
